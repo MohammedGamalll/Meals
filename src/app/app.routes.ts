91ac81046c453f83bcf07ff84429d5ac825c89cc
MohammedGamalll <11410120220283@stud.cu.edu.eg>
2025-02-11 01:58:09 +0200

    Fix errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   {
     path: 'meal/:id',
